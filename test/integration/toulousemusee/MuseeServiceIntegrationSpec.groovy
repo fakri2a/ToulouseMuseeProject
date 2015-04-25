c9a17f56b40d71d042fd9f90e6ae2a0f58a4ba75
Edsolat <edouard.sabourdy@laposte.net>
2015-04-25 12:02:56 +0200

    creation du test d'integration pour museeService

---
+++ test/integration/toulousemusee/MuseeServiceIntegrationSpec.groovy
@@ -0,0 +1,22 @@
+package toulousemusee
+
+
+
+import spock.lang.*
+
+/**
+ *
+ */
+class MuseeServiceIntegrationSpec extends Specification {
+
+    MuseeService museeService
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
