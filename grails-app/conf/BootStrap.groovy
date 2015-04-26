6722748526ba21c00f14f0df4330e3d2bc1901e4
Edsolat <edouard.sabourdy@laposte.net>
2015-04-26 11:19:50 +0200

    creation du service et controller pour les favoris. Test en cours d'écriture.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -3,9 +3,11 @@ import toulousemusee.ParseurCsvService
 class BootStrap {
 
     ParseurCsvService parseurCsvService
+
     def init = { servletContext ->
         parseurCsvService.parse("grails-app/ressources/Musee.csv")
     }
+
     def destroy = {
     }
 }
