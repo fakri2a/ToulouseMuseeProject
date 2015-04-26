1e5af3a99c4416888a529e03c1f268b2ed9750b7
Edsolat <edouard.sabourdy@laposte.net>
2015-04-26 02:21:57 +0200

    inittialisation dans le bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,10 @@
+import toulousemusee.ParseurCsvService
+
 class BootStrap {
 
+    ParseurCsvService parseurCsvService
     def init = { servletContext ->
+        parseurCsvService.parse("grails-app/ressources/Musee.csv")
     }
     def destroy = {
     }
